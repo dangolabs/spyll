dbd1da653a1d0762b48e1961ea01fd32f9191a17
Tinos Psomadakis <46272051+pTinosq@users.noreply.github.com>
2019-09-12 22:20:46 +0100

    Hello World
        - Added core features
        - Added search feature
        - Added count feature
        - Added read feature
        - Added write feature
        - Added help command
        - Added changelog command

---
+++ __init__.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python
+from .spyll import *
+import logging
+logging.getLogger(__name__).info("Made using Spyll")
+__author__="Tinos Psomadakis"
+__copyright__="Copyright (c) 2019 Tinos Psomadakis"
+__credits__ = [""]
+__version__ = "0.1"
+__maintainer__ = "Tinos Psomadakis"
+__email__ = "me@hellotinos.com"
+__status__ = "Alpha"
+
+def help():
+    print(
+    """
+
+    Commands:
+
+    spyll.write(file, text, split)
+
+    file - A string that defines the location of your text file
+    text - What you would like to write into the text file. Accepts list, tuple and string format.
+    split - Only available for list and tuple. This will determine how each element from the list/tuple will be written.
+
+    Example:
+
+    spyll.write(file="Hello.txt", text=["Hello","World",":)"], split="\n")
+    spyll.write(file="Hello.txt", text="Hello World :)")
+
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    spyll.read(file, split)
+
+    file - A string that defines the location of your text file
+    split - Will split the read text file into whatever string is provided
+
+    Example:
+
+    spyll.read(file="Hello.txt")
+    spyll.read(file="Hello.txt", split=" ")
+
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    spyll.search(file, text)
+
+    file - A string that defines the location of your text file
+    text - The value that you'd like to search for in a text file
+
+    Returns:
+
+    Boolean
+
+    Example:
+
+    spyll.search(file="Hello.txt", "example")
+
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    spyll.count(file, text)
+
+    file - A string that defines the location of your text file
+    text - The value that you'd like to count in a text file
+
+    Returns:
+
+    Integer
+
+    Example:
+
+    spyll.count(file="Hello.txt", " ")
+
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+
+    """
+
+
+
+
+    )
+
+def changelog():
+    print(
+    '''
+    Currently running spyll Version 0.1
+    Changelog:
+
+    - Hello World
+    - Added core features
+    - Added search feature
+    - Added count feature
+    - Added read feature
+    - Added write feature
+    - Added help command
+    - Added changelog command
+
+    '''
+    )
---
+++ spyll.py
@@ -0,0 +1,105 @@
+import os
+
+def read(file=None, split=None):
+
+    os.chdir(os.getcwd())
+
+    if file == None:
+        raise FileNotFoundError("You haven't specified a file. Usage:\n spyll.read(file=\"directory_to_file\")")
+        return
+
+    file = str(file)
+    with open(file,"r") as file:
+        text = file.read()
+        if split == None or split == False:
+            pass
+
+        else:
+            text = text.split(str(split))
+
+
+    return text
+
+def search(file=None, text=None):
+
+    os.chdir(os.getcwd())
+
+    if file == None:
+        raise FileNotFoundError("You haven't specified a file. Usage:\n spyll.search(file=\"directory_to_file\")")
+        return
+
+    if text == None:
+        raise ValueError("You haven't specified any text to search for. Usage:\n spyll.search(file='directory_to_file', text='Hello World')")
+        return
+
+    file = str(file)
+    with open(file,"r") as file:
+        read_text = file.read()
+        if text in read_text:
+            return True
+        else:
+            return False
+
+def count(file=None, text=None):
+
+    os.chdir(os.getcwd())
+
+    if file == None:
+        raise FileNotFoundError("You haven't specified a file. Usage:\n spyll.count(file=\"directory_to_file\")")
+        return
+
+    if text == None:
+        raise ValueError("You haven't specified any text to count. Usage:\n spyll.count(file='directory_to_file', text=' ')")
+        return
+
+    file = str(file)
+    with open(file,"r") as file:
+        read_text = file.read()
+        return read_text.count(text)
+
+
+def write(file=None, text=None, split=None):
+
+    os.chdir(os.getcwd())
+
+    file = str(file)
+
+    if file == None:
+        raise FileNotFoundError("You haven't specified a file. Usage:\n spyll.write(file=\"directory_to_file\")")
+        return
+
+    if text == None:
+        raise ValueError("You haven't specified any text. Usage:\n spyll.write(file='directory_to_file', text='Hello World')")
+        return
+
+    is_list=False
+
+    if type(text) == list or type(text) == tuple:
+        is_list=True
+
+    if split != None and is_list != True:
+        raise ValueError("You can only split inputs of type list. Usage: spyll.write(file='directory.txt', text=['hello','world'], split='\\n')")
+        return
+
+    if split != None and is_list:
+        newtext = ""
+        for i in text:
+            if len(text) - (text.index(i)) == 1:
+                newtext+=f"{i}"
+            else:
+                newtext+=f"{i}{split}"
+        text=newtext
+
+    if split == None and is_list:
+        newtext = ""
+        for i in text:
+            if len(text) - (text.index(i)) == 1:
+                newtext+=f"{i}"
+            else:
+                newtext+=f"{i} "
+        text=newtext
+
+
+
+    with open(file,"w") as file:
+        file.write(text)
