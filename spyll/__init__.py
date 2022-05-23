f65509bd608eecaa4885f3a7497043d70d8bdd65
Constantinos Psomadakis <tinosps@gmail.com>
2022-05-23 17:10:45 +0100

    Versioning and upload issues fixed

--- spyll/__init__.py
+++ spyll/__init__.py
@@ -3,7 +3,7 @@ from .spyll import *
 __author__ = "Dango Labs"
 __copyright__ = "Copyright (c) 2019 pTinosq"
 __credits__ = [""]
-__version__ = "1.2.1"
+__version__ = "1.2.2"
 __maintainer__ = "pTinosq"
 __email__ = "info@dangolabs.com"
 __status__ = "Release"
